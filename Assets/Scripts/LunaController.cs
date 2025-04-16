d79d60bb7f0f924b7d76458aa1338cd102064954
khoo <auld2002@gmail.com>
2025-04-16 17:16:09 +0800

    move

--- Assets/Scripts/LunaController.cs
+++ Assets/Scripts/LunaController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class LunaController : MonoBehaviour
 {
+    public float speed = 5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,9 @@ public class LunaController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float moveX = Input.GetAxis("Horizontal"); // A/D 或 左右键
+        Vector3 position = transform.position;
+        position.x += moveX * speed * Time.deltaTime;
+        transform.position = position;
     }
 }
