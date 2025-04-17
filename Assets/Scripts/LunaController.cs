8f07beec1f63243f04e20eac547df33d3aa980c4
aoooxyz <auld2002@gmail.com>
2025-04-17 23:59:05 +0800

    map

--- Assets/Scripts/LunaController.cs
+++ Assets/Scripts/LunaController.cs
@@ -8,19 +8,17 @@ public class LunaController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Application.targetFrameRate = 120;  //设置游戏帧率为120
     }
 
     // Update is called once per frame
     void Update()
     {
-        float moveX = Input.GetAxisRaw("Horizontal");  //定义浮点数moveX用于存储Input.GetAxis方法的返回值，控制左右
-	    float moveY = Input.GetAxisRaw("Vertical");
+        float horizontal = Input.GetAxisRaw("Horizontal");  //定义浮点数horizontal用于存储Input.GetAxis方法的返回值，控制左右
+	    float vertical = Input.GetAxisRaw("Vertical");  //定义浮点数vertical用于存储Input.GetAxis方法的返回值，控制上下
         Vector3 position = transform.position;  //定义二维向量类型坐标posttion并赋值获取当前对象物体的位置
-	    Vector3 forward = transform.forward;  //定义二维向量类型forward并赋值获取当前对象物体的朝向
-        position.y += moveY * speed * Time.deltaTime;  //计算垂直位移量:方向*速度*每帧耗时，使用Time.deltaTime实现帧率无关的平滑移动
-        position.x += moveX * speed * Time.deltaTime;  //计算水平位移量:方向*速度*每帧耗时，使用Time.deltaTime实现帧率无关的平滑移动
+        position.x += 6*horizontal*Time.deltaTime;  //根据时间差和水平方向的输入，更新y坐标
+        position.y += 6*vertical*Time.deltaTime;  //根据时间差和垂直方向的输入，更新x坐标
 	    transform.position = position;  //把修改后的位置应用到对象物体上，实现移动效果
-        transform.forward = forward;  //把修改后的位置应用到对象物体上，实现移动效果
     }
 }
